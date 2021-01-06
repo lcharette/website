e0b704cd77008d7ea0eda807864db26262782f8d
Louis Charette <charette.louis@gmail.com>
2021-01-06 12:34:58 -0500

    Implement dark mode switch + put FA & Darkmode CSS/JS globslly using the twig vars

--- themes/quark-custom/quark-custom.php
+++ themes/quark-custom/quark-custom.php
@@ -5,5 +5,33 @@ use Grav\Common\Theme;
 
 class QuarkCustom extends Quark
 {
-    // Access plugin events in this class
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+        ];
+    }
+    
+    /**
+     * Set needed variables to display the search results.
+     *
+     * @return void
+     */
+    public function onTwigSiteVariables()
+    {
+        $twig = $this->grav['twig'];
+
+        // Add FontAwesome Globally
+        if ($this->config->get('theme.include_fontawesome')) {
+            $this->grav['assets']->add('theme://css/fontAwesome.css');
+        }
+
+        if ($this->config->get('theme.dark_mode')) {
+            $this->grav['assets']->add('theme://css/darkMode.css');
+            $this->grav['assets']->addJs('theme://js/darkMode.js', ['group' => 'bottom']);
+        }
+    }
 }
